aa4a0562347e22263fa6a04367f074093bb390c7
lizenan <32972438+Lizn-zn@users.noreply.github.com>
2023-02-27 09:33:00 +0800

    Update run.sh

--- HWF/run.sh
+++ HWF/run.sh
@@ -17,17 +17,17 @@ CUDA_VISIBLE_DEVICES=4 python3 sl_training.py --seed 0 --exp_name mi --T 1e-6
 
 ##========================================================================================
 
-# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_ngs --net 'checkpoint/final_results/sym_net_0_ngs_6-BS_0.t7'
+# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_ngs --net 'checkpoint/sym_net_0_ngs_6-BS_0.t7'
 
-# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_log --net 'checkpoint/final_results/lenet_0_mi_0.t7'
+# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_log --net 'checkpoint/lenet_0_mi_0.t7'
 
-# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_log --net 'checkpoint/final_results/lenet_0_sl_0.t7'
+# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_log --net 'checkpoint/lenet_0_sl_0.t7'
 
-# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_log --net 'checkpoint/final_results/lenet_0_fs_log_best_0.t7'
+# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_log --net 'checkpoint/lenet_0_fs_log_best_0.t7'
 
-# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_exp --net 'checkpoint/final_results/lenet_0_fs_exp_best_0.t7' --data_shuffle True
+# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_exp --net 'checkpoint/lenet_0_fs_exp_best_0.t7' --data_shuffle True
 
-# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_linear --net 'checkpoint/final_results/lenet_0_fs_linear_best_0.t7'  --data_shuffle True
+# CUDA_VISIBLE_DEVICES=4 python3 bs_training.py --seed 0 --exp_name bs_linear --net 'checkpoint/lenet_0_fs_linear_best_0.t7'  --data_shuffle True
 
 
 
